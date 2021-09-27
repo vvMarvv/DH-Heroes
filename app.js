b1d6ebe8c5a728a87ae5fff6e1f08e78d94bfacc
Maria <mmariabelenn@gmail.com>
2021-09-27 20:42:03 -0300

    primer commit

--- app.js
+++ app.js
@@ -3,16 +3,36 @@ const app = express();
 const PORT = 3030;
 const path = require('path')
 
-
-app.get('/', (req, res) => {
+app.get('localhost:3030/home', (req, res) => {
     res.sendFIle(path.join(__dirname, "/views/index.html"))
 });
 
 app.get('/babbage', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/babbage.html"))
 })
+
 app.get('/berners-lee', (req, res)  => {
     res.sendFile(path.join(__dirname, "/views/berners-lee.html"))
+})
+
+app.get('/clarke', (req, res)  => {
+    res.sendFile(path.join(__dirname, "/views/clarke.html"))
+})
+
+app.get('/hamilton', (req, res)  => {
+    res.sendFile(path.join(__dirname, "/views/hamilton.html"))
+})
+
+app.get('/hopper', (req, res)  => {
+    res.sendFile(path.join(__dirname, "/views/hopper.html"))
+})
+
+app.get('/lovelace', (req, res)  => {
+    res.sendFile(path.join(__dirname, "/views/lovelace.html"))
+})
+
+app.get('/turing', (req, res)  => {
+    res.sendFile(path.join(__dirname, "/views/turing.html"))
 })
 
 
